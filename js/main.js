9e33efd029502726f1a714722a34db01a06cdcce
sinadaryaei <sinatangssiran@gmail.com>
2024-01-08 01:05:39 +0000

    Add search functionality to Pokemon viewer, see #1 for details

--- js/main.js
+++ js/main.js
@@ -65,16 +65,69 @@ fetchPokemons();
 
 document.getElementById('search-bar').addEventListener('input', function (e) {
   var searchText = e.target.value; // Get the text from the search bar
-
+  var pokeContainer = document.getElementById('poke-container');
   var pokemon = pokeContainer.getElementsByClassName('pokemon'); // Get all the Pokemon
 
   for (var i = 0; i < pokemon.length; i++) {
-    var name = pokemon[i].getElementsByClassName('info')[0].getElementsByClassName('name')[0].innerText;
-    // If the Pokemon's name includes the search text, show it, otherwise hide it
-    if (name.toLowerCase().includes(searchText.toLowerCase())) {
+    var name = pokemon[i].getElementsByClassName('info')[0].getElementsByClassName('name')[0].innerText; // Get the name of the Pokemon
+    var number = pokemon[i].getElementsByClassName('info')[0].getElementsByClassName('number')[0].innerText; // Get the number of the Pokemon
+
+    // Remove the leading '#' from the number and convert it to a number
+    var numberAsNumber = Number(number.replace('#', ''));
+
+    // Convert the search text to a number
+    var searchTextAsNumber = Number(searchText);
+
+    // If the Pokemon's name includes the search text, or the number equals the search text, show it, otherwise hide it
+    if (name.toLowerCase().includes(searchText.toLowerCase()) || numberAsNumber === searchTextAsNumber) {
       pokemon[i].style.display = '';
     } else {
       pokemon[i].style.display = 'none';
     }
+  }
+});
+
+// ... rest of your code ...
+
+var suggestionsContainer = document.createElement('div');
+suggestionsContainer.id = 'suggestions-container';
+document.body.appendChild(suggestionsContainer);
+
+document.getElementById('search-bar').addEventListener('input', function (e) {
+  var searchText = e.target.value; // Get the text from the search bar
+  var pokeContainer = document.getElementById('poke-container');
+  var pokemon = pokeContainer.getElementsByClassName('pokemon'); // Get all the Pokemon
+
+  // Clear the current suggestions
+  suggestionsContainer.innerHTML = '';
+
+  for (var i = 0; i < pokemon.length; i++) {
+    var name = pokemon[i].getElementsByClassName('info')[0].getElementsByClassName('name')[0].innerText; // Get the name of the Pokemon
+    var number = pokemon[i].getElementsByClassName('info')[0].getElementsByClassName('number')[0].innerText; // Get the number of the Pokemon
+
+    // Remove the leading '#' from the number and convert it to a number
+    var numberAsNumber = Number(number.replace('#', ''));
+
+    // Convert the search text to a number
+    var searchTextAsNumber = Number(searchText);
+
+    // If the Pokemon's name includes the search text, or the number equals the search text, add it to the suggestions
+    if (name.toLowerCase().includes(searchText.toLowerCase()) || numberAsNumber === searchTextAsNumber) {
+      var suggestion = document.createElement('div');
+      suggestion.innerText = name + ' ' + number;
+      suggestionsContainer.appendChild(suggestion);
+
+      suggestion.addEventListener('click', function () {
+        document.getElementById('search-bar').value = this.innerText;
+        suggestionsContainer.style.display = 'none';
+      });
+    }
+  }
+
+  // If the search bar is cleared, hide the suggestions container
+  if (searchText === '') {
+    suggestionsContainer.style.display = 'none';
+  } else {
+    suggestionsContainer.style.display = 'block';
   }
 });
